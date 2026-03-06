e12d2b9d51f655050f4b2071d01a3bbaa537c444
adityaraaj123478-droid <adityaraaj123478@gmail.com>
2026-03-06 15:14:34 +0530

    UC11: Object-Oriented Palindrome implemented in PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,67 +1,28 @@
 import java.util.Scanner;
+import java.util.Stack;
 
-class Node {
-    char data;
-    Node next;
+class PalindromeChecker {
+    private String str;
 
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class SinglyLinkedList {
-    Node head;
-
-    void add(char data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null)
-            temp = temp.next;
-        temp.next = newNode;
+    public PalindromeChecker(String str) {
+        this.str = str;
     }
 
-    Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
-    }
+    public boolean checkPalindrome() {
+        String normalized = str.replaceAll("\\s+", "").toLowerCase();
+        Stack<Character> stack = new Stack<>();
 
-    boolean isPalindrome() {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        for (char c : normalized.toCharArray()) {
+            stack.push(c);
         }
 
-        Node secondHalf = reverse(slow.next);
-        Node firstHalf = head;
-        Node copySecondHalf = secondHalf;
-
-        boolean result = true;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                result = false;
-                break;
+        for (char c : normalized.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
             }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
         }
 
-        slow.next = reverse(copySecondHalf);
-        return result;
+        return true;
     }
 }
 
@@ -70,12 +31,9 @@ public class PalindromeCheckerApp {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        SinglyLinkedList list = new SinglyLinkedList();
-        for (int i = 0; i < input.length(); i++) {
-            list.add(input.charAt(i));
-        }
+        PalindromeChecker checker = new PalindromeChecker(input);
 
-        if (list.isPalindrome()) {
+        if (checker.checkPalindrome()) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
