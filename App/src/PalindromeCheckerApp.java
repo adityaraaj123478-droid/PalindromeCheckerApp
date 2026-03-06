28b5c76a9f9ca3c05d99c790168f3f8e140bbd60
adityaraaj123478-droid <adityaraaj123478@gmail.com>
2026-03-06 15:23:05 +0530

    UC12: Strategy Pattern for Palindrome implemented in PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,81 +1,59 @@
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
-class Node {
-    char data;
-    Node next;
+// Strategy interface
+interface PalindromeStrategy {
+    boolean isPalindrome(String str);
+}
 
-    Node(char data) {
-        this.data = data;
-        this.next = null;
+// Stack-based strategy
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) stack.push(c);
+        for (char c : str.toCharArray()) if (c != stack.pop()) return false;
+        return true;
     }
 }
 
-class SinglyLinkedList {
-    Node head;
-
-    void add(char data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null)
-            temp = temp.next;
-        temp.next = newNode;
-    }
-
-    Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
-    }
-
-    boolean isPalindrome() {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow.next);
-        Node firstHalf = head;
-        Node copySecondHalf = secondHalf;
-
-        boolean result = true;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                result = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        slow.next = reverse(copySecondHalf);
-        return result;
+// Deque-based strategy
+class DequeStrategy implements PalindromeStrategy {
+    @Override
+    public boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : str.toCharArray()) deque.addLast(c);
+        while (deque.size() > 1) if (deque.removeFirst() != deque.removeLast()) return false;
+        return true;
     }
 }
 
+// Context class
+class PalindromeChecker {
+    private PalindromeStrategy strategy;
+
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean check(String str) {
+        return strategy.isPalindrome(str);
+    }
+}
+
+// Main app
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        SinglyLinkedList list = new SinglyLinkedList();
-        for (int i = 0; i < input.length(); i++) {
-            list.add(input.charAt(i));
-        }
-
-        if (list.isPalindrome()) {
+        // Choose strategy dynamically (example: Stack)
+        PalindromeChecker checker = new PalindromeChecker(new StackStrategy());
+        if (checker.check(input)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
